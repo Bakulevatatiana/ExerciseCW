e1b12505dcdf859d0b342b9f0b8a072267721b20
bakuleva <t.kovantseva@mail.ru>
2022-10-12 13:47:26 +0700

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ class StringPalindrome {
         buffer.reverse();
         String data = buffer.toString();
         if(myString.equals(data)){
-            System.out.println("Строка является полиндромом ");
+            System.out.println("Строка является полиндромом");
         }else{
             System.out.println("Строка не является полидромом");
         }
